43bbd6d8038f0a1b2a286524b987a9443efdff3b
rakshitha-prabhuswamy <166089290+rakshitha-prabhuswamy@users.noreply.github.com>
2026-06-18 22:41:29 +0530

    Delete

--- sql/create_bugs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE bugs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    bug_name VARCHAR(255),
-    bug_type VARCHAR(255),
-    bug_priority VARCHAR(50),
-    project_name VARCHAR(255),
-    bug_level VARCHAR(50),
-    start_date VARCHAR(50)
-);
